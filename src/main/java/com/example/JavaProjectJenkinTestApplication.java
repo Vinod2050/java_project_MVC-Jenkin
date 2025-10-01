63ea8fd66215a82a6b5ea4afd08ee6f474c90538
Vinod2050 <vinodmache7@gmail.com>
2025-10-01 15:20:47 +0530

    first Commit

---
+++ src/main/java/com/example/JavaProjectJenkinTestApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectJenkinTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectJenkinTestApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Welcome Page</title>
+</head>
+<body>
+<h1>${message}</h1>
+<p>This is the basic landing page of your Spring Boot JSP application.</p>
+</body>
+</html>
