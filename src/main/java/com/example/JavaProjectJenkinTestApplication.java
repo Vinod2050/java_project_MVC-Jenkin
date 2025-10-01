b0377917dc75825ea1ad8f98e19146b476321d5d
Vinod2050 <vinodmache7@gmail.com>
2025-10-01 17:28:12 +0530

    packaging war added in pom

--- src/main/java/com/example/JavaProjectJenkinTestApplication.java
+++ src/main/java/com/example/JavaProjectJenkinTestApplication.java
@@ -2,12 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavaProjectJenkinTestApplication {
+public class JavaProjectJenkinTestApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaProjectJenkinTestApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JavaProjectJenkinTestApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProjectJenkinTestApplication.class, args);
+    }
 }
+
